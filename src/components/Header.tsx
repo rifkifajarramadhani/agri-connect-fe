8552dd800632a8d7f734d5dbf27650d87a5d2d16
Rifki Fajar Ramadhani <rifkid56@gmail.com>
2026-03-17 14:18:56 +0700

    feat: update Header component styles and integrate color variables in CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,21 @@ import { Bell, Mail } from 'lucide-react';
 
 const Header = () =>  {
   return (
-    <header className="flex justify-between items-center p-4">
+    <header className="bg-brand-green flex justify-between items-center p-4">
       <img src={logo} alt="Agri Connect logo" width={48} height={48} />
 
       <nav>
         <ul className="flex space-x-4">
-          <li><a href="#" className="text-gray-700 hover:text-green-500">Home</a></li>
-          <li><a href="#" className="text-gray-700 hover:text-green-500">Network</a></li>
-          <li><a href="#" className="text-gray-700 hover:text-green-500">Marketplace</a></li>
-          <li><a href="#" className="text-gray-700 hover:text-green-500">Forum</a></li>
+          <li><a href="#" className="text-beige hover:text-green-500">Home</a></li>
+          <li><a href="#" className="text-beige hover:text-green-500">Network</a></li>
+          <li><a href="#" className="text-beige hover:text-green-500">Marketplace</a></li>
+          <li><a href="#" className="text-beige hover:text-green-500">Forum</a></li>
         </ul>
       </nav>
 
       <div className="flex justify-between items-center gap-4">
-        <Bell size={24} />
-        <Mail size={24} />
+        <Bell color='beige' size={24} />
+        <Mail color='beige' size={24} />
         <img src="https://placehold.co/48?text=Me" alt="Profile" className='rounded-full' />
       </div>
     </header>
