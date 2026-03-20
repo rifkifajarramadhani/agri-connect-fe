72515cd027a734fe52bd8206a35701eafedcc5c9
Rifki Fajar Ramadhani <rifkid56@gmail.com>
2026-03-20 20:53:34 +0700

    feat: implement login form and update login page layout with logo and welcome message

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,5 @@
+import logo from "@/assets/logo-green.png";
+import { LoginForm } from "@/components/login-form";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
@@ -5,5 +7,16 @@ export const Route = createFileRoute("/login")({
 });
 
 function LoginPage() {
-  return <div>Hello "/login"!</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        <div className="flex flex-col items-center mb-6">
+          <img src={logo} alt="" className="mx-auto" height={96} width={96} />
+          <h2 className="mt-4 text-2xl font-bold">Welcome Back!</h2>
+          <h6>Sign In to Agri Connect!</h6>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
