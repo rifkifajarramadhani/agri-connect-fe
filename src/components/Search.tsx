a21fa69fdc6987a55abf2da19b366e606fda02f9
Rifki Fajar Ramadhani <rifkid56@gmail.com>
2026-03-20 14:30:10 +0700

    feat: create Input component for reusable input fields; update Search component to utilize new Input component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,42 +1,45 @@
 import { useForm } from "@tanstack/react-form";
 import { FieldInfo } from "./FieldInfo";
+import { Input } from "./ui/input";
 
 const Search = () => {
   interface SearchForm {
     query: string;
   }
   const defaultSearchValues: SearchForm = {
-    query: '',
-  }
+    query: "",
+  };
 
   const formOptions = {
     defaultValues: defaultSearchValues,
-  }
+  };
   const form = useForm({
     ...formOptions,
     onSubmit: (values) => {
-      console.log('Search submitted:', values);
-    }
-  })
+      console.log("Search submitted:", values);
+    },
+  });
 
-  return <div>
-    <form.Field
-      name="query"
-      children={(field) => (
-        <>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={field.state.value}
-            onBlur={field.handleBlur}
-            onChange={(e) => field.handleChange(e.target.value)}
-            className="bg-white rounded-md px-2 py-2"
-          />
-          <FieldInfo field={field} />
-        </>
-      )}
-    />
-  </div>
-}
+  return (
+    <div>
+      <form.Field
+        name="query"
+        children={(field) => (
+          <>
+            <Input
+              type="text"
+              placeholder="Search..."
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(e.target.value)}
+              className="bg-white opacity-75"
+            />
+            <FieldInfo field={field} />
+          </>
+        )}
+      />
+    </div>
+  );
+};
 
 export default Search;
